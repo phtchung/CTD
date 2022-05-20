8632b1d6383011b394a21d8170d09d8d7cb10c37
phtchung <phamthanhchung2001@gmail.com>
2022-05-20 07:46:08 +0700

    W4_2

---
+++ W4_2/run.bat
@@ -0,0 +1,14 @@
+@echo off
+.\cmake-build-debug\untitled.exe .\cmake-build-debug\example1.kpl > temp
+fc /a temp .\cmake-build-debug\result1.txt
+.\cmake-build-debug\untitled.exe .\cmake-build-debug\example2.kpl > temp
+fc /a temp .\cmake-build-debug\result2.txt
+.\cmake-build-debug\untitled.exe .\cmake-build-debug\example3.kpl > temp
+fc /a temp .\cmake-build-debug\result3.txt
+.\cmake-build-debug\untitled.exe .\cmake-build-debug\example4.kpl > temp
+fc /a temp .\cmake-build-debug\result4.txt
+.\cmake-build-debug\untitled.exe .\cmake-build-debug\example5.kpl > temp
+fc /a temp .\cmake-build-debug\result5.txt
+.\cmake-build-debug\untitled.exe .\cmake-build-debug\example6.kpl > temp
+fc /a temp .\cmake-build-debug\result6.txt
+pause
